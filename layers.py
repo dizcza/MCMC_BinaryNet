fixed bug in forward sign()

layers.py
@@ -21,31 +21,52 @@ class BinaryFunc(torch.autograd.Function):
 
 class BinaryLinear(nn.Linear):
 
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__(in_features, out_features, bias)
+        matrix_proba = torch.FloatTensor(self.weight.data.shape).fill_(0.5)
+        self.weight.data = torch.bernoulli(matrix_proba) * 2 - 1
+        self.weight_clone = self.weight.clone()
+
     def forward(self, x):
         x_mean = torch.mean(torch.abs(x))
-        x = F.linear(BinaryFunc.apply(x), self.weight.sign(), self.bias)
+        x = BinaryFunc.apply(x)
+        self.weight_clone = self.weight.clone()
+        self.weight.data.sign_()
+        x = F.linear(x, self.weight, self.bias)
+        self.weight.data = self.weight_clone.data
         x = F.mul(x, x_mean)
         return x
 
-    def parameters(self):
-        for p in super().parameters():
-            p.is_binary = True
-            yield p
+    def named_parameters(self, memo=None, prefix=''):
+        for name, param in super().named_parameters(memo, prefix):
+            param.is_binary = True
+            yield name, param
 
 
 class BinaryConv2d(nn.Conv2d):
 
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super().__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        matrix_proba = torch.FloatTensor(self.weight.data.shape).fill_(0.5)
+        self.weight.data = torch.bernoulli(matrix_proba) * 2 - 1
+        self.weight_clone = self.weight.clone()
+
     def forward(self, x):
         x_mean = torch.mean(torch.abs(x))
-        x = F.conv2d(BinaryFunc.apply(x), self.weight.sign(), self.bias, self.stride,
+        x = BinaryFunc.apply(x)
+        self.weight_clone = self.weight.clone()
+        self.weight.data.sign_()
+        x = F.conv2d(x, self.weight, self.bias, self.stride,
                      self.padding, self.dilation, self.groups)
+        self.weight.data = self.weight_clone.data
         x = F.mul(x, x_mean)
         return x
 
-    def parameters(self):
-        for p in super().parameters():
-            p.is_binary = True
-            yield p
+    def named_parameters(self, memo=None, prefix=''):
+        for name, param in super().named_parameters(memo, prefix):
+            param.is_binary = True
+            yield name, param
 
 
 class ScaleFunc(torch.autograd.Function):

mlp.py
@@ -15,8 +15,8 @@ class NetBinary(nn.Module):
         conv_layers = []
         for (in_features, out_features) in zip(conv_channels[:-1], conv_channels[1:]):
             conv_layers.append(nn.BatchNorm2d(in_features))
-            conv_layers.append(BinaryConv2d(in_features, out_features, kernel_size=3, padding=1, bias=False))
-            conv_layers.append(nn.PReLU(out_features))
+            conv_layers.append(BinaryConv2d(in_features, out_features, kernel_size=5, padding=2, bias=False))
+            conv_layers.append(nn.PReLU())
         self.conv_sequential = nn.Sequential(*conv_layers)
 
         fc_in_features = 28 ** 2 * conv_channels[-1]
@@ -25,7 +25,7 @@ class NetBinary(nn.Module):
         for (in_features, out_features) in zip(fc_sizes[:-1], fc_sizes[1:]):
             fc_layers.append(nn.BatchNorm1d(in_features))
             fc_layers.append(BinaryLinear(in_features, out_features, bias=False))
-            fc_layers.append(nn.PReLU(out_features))
+            fc_layers.append(nn.PReLU())
         self.fc_sequential = nn.Sequential(*fc_layers)
         self.scale_layer = ScaleLayer()
 
@@ -33,7 +33,11 @@ class NetBinary(nn.Module):
         return type(self).__name__
 
     def parameters_binary(self):
-        return filter(lambda param: getattr(param, "is_binary", False), self.parameters())
+        for name, param in self.named_parameters_binary():
+            yield param
+
+    def named_parameters_binary(self):
+        return filter(lambda named_param: getattr(named_param[1], "is_binary", False), self.named_parameters())
 
     def forward(self, x):
         x = self.conv_sequential(x)
@@ -43,17 +47,17 @@ class NetBinary(nn.Module):
         return x
 
 
-def train_binary(n_epoch=10):
-    conv_channels = [1, 2]
-    fc_sizes = [1024, 10]
+def train_binary(n_epoch=100):
+    conv_channels = [1, 10, 20]
+    fc_sizes = [320, 50, 10]
     model = NetBinary(conv_channels, fc_sizes)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-    scheduler = StepLRClamp(optimizer, step_size=1, gamma=0.5, min_lr=1e-6)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    scheduler = StepLRClamp(optimizer, step_size=2, gamma=0.5, min_lr=1e-6)
     trainer = Trainer(model, criterion, optimizer, scheduler)
     trainer.train(n_epoch, debug=True)
 
 
 if __name__ == '__main__':
     train_binary()
-    test()
+    # test()

trainer.py
@@ -70,9 +70,12 @@ class Trainer(object):
     @staticmethod
     def load_model(model_name) -> Union[nn.Module, None]:
         model_path = os.path.join(MODELS_DIR, model_name + '.pt')
-        if os.path.exists(model_path):
+        if not os.path.exists(model_path):
+            return None
+        try:
             return torch.load(model_path)
-        else:
+        except Exception:
+            print("Couldn't load previously saved {}.pt model".format(model_name))
             return None
 
     def save_model(self):
@@ -82,8 +85,20 @@ class Trainer(object):
         torch.save(self.model, model_path)
         print("Saved to {}".format(model_path))
 
+    def optimizer_step_analyze(self) -> int:
+        param_sign_before = {}
+        for name, param in self.model.named_parameters_binary():
+            param_sign_before[name] = param.data.clone()
+        self.optimizer.step()
+        changed_signs = 0
+        # for name, param in self.model.named_parameters_binary():
+        #     print(name, float(param.grad.min()), float(param.grad.max()), float(param.data.min()), float(param.data.max()))
+        for name, param in self.model.named_parameters_binary():
+            changed_signs += torch.sum((param.data * param_sign_before[name]) < 0)
+        return changed_signs
+
     def train(self, n_epoch=10, debug=False):
-        train_loader = get_data_loader(train=True)
+        train_loader = get_data_loader(train=True, batch_size=128)
         log_step = len(train_loader) // 10
         use_cuda = torch.cuda.is_available()
         if use_cuda:
@@ -96,10 +111,16 @@ class Trainer(object):
         dataset_name = type(train_loader.dataset).__name__
         print("Training '{}'. Best {} train accuracy so far: {:.4f}".format(
             str(self.model), dataset_name, best_accuracy))
+
+        total_binary_params = 0
+        for param in self.model.parameters_binary():
+            total_binary_params += param.numel()
+
         for epoch in range(n_epoch):
+            changed_signs = 0
             if self.scheduler is not None:
                 self.scheduler.step()
-            for batch_id, (images, labels) in enumerate(train_loader):
+            for batch_id, (images, labels) in enumerate(train_loader, start=1):
                 images = Variable(images)
                 labels = Variable(labels)
                 if use_cuda:
@@ -110,14 +131,18 @@ class Trainer(object):
                 outputs = self.model(images)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
-                self.optimizer.step()
-                for param_binary in self.model.parameters_binary():
-                    param_binary.data.clamp_(min=-1, max=1)
+                changed_signs += self.optimizer_step_analyze()
+
+                # todo: do we actually need weight clipping?
+                for param in self.model.parameters_binary():
+                    param.data.clamp_(min=-1, max=1)
 
                 if batch_id % log_step == 0:
                     batch_accuracy = get_softmax_accuracy(outputs, labels)
-                    print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Batch accuracy: %.4f' % (
-                        epoch, n_epoch, batch_id, len(train_loader), loss.data[0], batch_accuracy))
+                    changed_signs_relative = changed_signs / (batch_id * total_binary_params)
+                    print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Batch accuracy: %.4f, Changed signs: %.2e' % (
+                           epoch, n_epoch, batch_id, len(train_loader), loss.data[0], batch_accuracy,
+                           changed_signs_relative))
             if not debug:
                 accuracy = calc_accuracy(self.model, train_loader)
                 if accuracy > best_accuracy:
@@ -130,6 +155,10 @@ def test():
     for model_name in os.listdir(MODELS_DIR):
         model_path = os.path.join(MODELS_DIR, model_name)
         test_loader = get_data_loader(train=False)
-        model = torch.load(model_path)
+        try:
+            model = torch.load(model_path)
+        except Exception:
+            print("Skipped {} model".format(model_path))
+            continue
         accur = calc_accuracy(model, test_loader)
         print("\t{}: {:.4f}".format(str(model), accur))